8b14594ba0d25ad85157b8efe7dea85180a5e494
anthony.s.flath <anthony.s.flath@gmail.com>
2026-09-12 18:38:16 -0400

    workflow

--- assets/js/mathjax-config.js
+++ assets/js/mathjax-config.js
@@ -1,13 +1,15 @@
 window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    macros: {
-      ZZ: "\\mathbb{Z}",
-      RR: "\\mathbb{R}",
-      QQ: "\\mathbb{Q}",
-      GL: "\\operatorname{GL}_n(\\mathbb{Z})",
-      GLk: "\\operatorname{GL}_k(\\mathbb{Z})",
-      Span: "\\operatorname{span}"
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        macros: {
+            ZZ: "\\mathbb{Z}",
+            RR: "\\mathbb{R}",
+            QQ: "\\mathbb{Q}",
+            GL: "\\operatorname{GL}_n(\\mathbb{Z})",
+            GLk: "\\operatorname{GL}_k(\\mathbb{Z})",
+            Span: "\\operatorname{span}",
+            norm: ["\\lVert #1 \\rVert", 1],
+            Norm: ["\\left\\lVert #1 \\right\\rVert", 1],
+        }
     }
-  }
 };
